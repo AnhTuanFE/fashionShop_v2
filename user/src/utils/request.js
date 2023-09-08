09ba6b4cd3ded0a0645165af8252496ad451d505
NguyenTriHai <nthai2001cr@gmail.com>
2023-09-08 16:39:38 +0700

    update get server api url

--- user/src/utils/request.js
+++ user/src/utils/request.js
@@ -5,10 +5,21 @@ const getToken = () => {
     return `Bearer ${JSON.parse(localStorage.getItem('userInfo'))?.accessToken}`;
 };
 
-export const API_FASHIONSHOP =
-    process.env.NODE_ENV == 'production'
-        ? process.env.REACT_APP_SERVER_API_PRODUCTION_URL
-        : process.env.REACT_APP_SERVER_API_DEVELOPMENT_URL;
+const getServerUrl = () => {
+    let serverUrl = 'http://localhost:5000/api/v1';
+    if (process.env.NODE_ENV == 'production') {
+        if (process.env.REACT_APP_SERVER_API_PRODUCTION_URL) {
+            serverUrl = String(process.env.REACT_APP_SERVER_API_PRODUCTION_URL);
+        }
+    } else {
+        if (process.env.REACT_APP_SERVER_API_DEVELOPMENT_URL) {
+            serverUrl = String(process.env.REACT_APP_SERVER_API_DEVELOPMENT_URL);
+        }
+    }
+    return serverUrl;
+};
+
+export const API_FASHIONSHOP = getServerUrl();
 
 const request = axios.create({
     baseURL: API_FASHIONSHOP,
